49bfb8326e530daa32d00f22dc936e45eac51429
Andrew Salmon <asalmon9989@gmail.com>
2018-05-17 15:35:57 -0700

    Fixed segmentation error and added small optimization when counting total primes

--- Sieve_of_Eratosthenes.cpp
+++ Sieve_of_Eratosthenes.cpp
@@ -1,57 +1,72 @@
+
 #include <iostream>
+#include "time.h"
 using namespace std;
 
 // https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
 
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
- *	Problems:												*
- *		receiving segmentation error when calculating with	*
- *		boundary above 10^7.								*
-  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+*	Problems:												*
+*		receiving segmentation error when calculating with	*
+*		boundary above 10^7.								*
+* * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 void SieveOfEratosthenes(long long int limit)
 {
 	long long int i;
 	long long int k;
 	long long int primeCount;
-	bool isPrime[limit + 1];
+	bool * isPrime = new bool[limit + 1];
+
 
 	memset(isPrime, true, sizeof(isPrime));
 	isPrime[0] = false;
 	isPrime[1] = false;
 
+	primeCount = limit - 1;
+
 	for (i = 2; i*i <= limit; i++)
 	{
 		if (isPrime[i])
 		{
-			for (k = i*i; k <= limit; k += i)
+			for (k = i * i; k <= limit; k += i)
 			{
+				if (isPrime[k])
+				{
+					primeCount--;
+				}
 				isPrime[k] = false;
 			}
 		}
 	}
+	
+	/*
 	primeCount = 0;
 	for (i = 2; i <= limit; i++)
 	{
 		if (isPrime[i])
 		{
-			primeCount ++;
+			primeCount++;
 		}
 	}
+	*/
 	cout << "\nThere are " << primeCount << " prime numbers between 2 and "
-		 << limit << ".\n\n";
+		<< limit << ".\n\n";
+	delete[] isPrime;
 }
 
-long long int upperBound;
+
 
 int main()
 {
+	long long int upperBound;
 	cout << "Enter upper bound: ";
-	cin  >> upperBound;
+	cin >> upperBound;
 
 	cout << "Finding all prime numbers below " << upperBound
-		 << "...\n";
+		<< "...\n";
 
 	SieveOfEratosthenes(upperBound);
+	system("PAUSE");
 
 	return 0;
 }
